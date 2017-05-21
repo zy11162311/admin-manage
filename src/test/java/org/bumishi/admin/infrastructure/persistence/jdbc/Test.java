f092f45cdf83d6ded959159e2d1b6a372ad47408
zhang.yuan7 <zhang.yuan7@byd.com>
2017-05-21 16:06:02 +0800

    first commit i#	新文件：   .gitignore

---
+++ src/test/java/org/bumishi/admin/infrastructure/persistence/jdbc/Test.java
@@ -0,0 +1,26 @@
+//package org.bumishi.admin.infrastructure.persistence.jdbc;
+//
+//import org.bumishi.admin.domain.repository.ResourceRepository;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+///**
+// * @author qiang.xie
+// * @date 2016/10/11
+// */
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+//public class Test {
+//
+//    @Autowired
+//    ResourceRepository roleRepository;
+//
+//    @org.junit.Test
+//    public void contains(){
+//        //roleRepository.c("a");
+//    }
+//
+//}
